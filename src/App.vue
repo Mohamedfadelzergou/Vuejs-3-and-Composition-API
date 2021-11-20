33a17d4fccb48a04dfccfc90cd9895f575b888bc
Mohamedfadelzergou <mohamedfadelzergou@gmail.com>
2021-11-20 23:09:46 +0100

    use Using useRouter (Navigability) in vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
+    <router-link to="/blog">Blog</router-link>|
     <router-link to="/about">About</router-link>
+  </div>
+  <div>
+    <button @click="Previous">Previous</button>
+    <button @click="Next">Next</button>
+    <button @click="Redirect">Redirect</button>
   </div>
   <router-view/>
 </template>
-
+<script>
+import { useRouter } from 'vue-router'
+export default {
+  setup() {
+    const router = useRouter()
+    const Previous =()=>{
+     router.go(-1);
+    }
+    const Next =()=>{
+      router.go(+1);
+    }
+    const Redirect =()=>{
+      router.push('/blog');
+    }
+    return {Previous,Next,Redirect}
+  },
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
