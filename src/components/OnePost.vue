964dc1367ab6a708544d81a6b4048eba453c6c8b
Mohamedfadelzergou <mohamedfadelzergou@gmail.com>
2021-11-20 22:22:09 +0100

    use Props in the Setup Function

---
+++ src/components/OnePost.vue
@@ -0,0 +1,19 @@
+<template>
+        <h2>{{post.title}}</h2>
+        <p>{{extract}}</p>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+    props:['post'],
+    setup(props){
+        const extract = computed(()=>props.post.content.substring(0,50)+'...');
+        return {extract}
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Post.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>List of Posts</h1>
+    <div v-for="post in posts" :key="post.id">
+        <one-post :post="post"/>
+    </div>
+</template>
+<script>
+import { ref } from 'vue'
+import OnePost from '../components/OnePost.vue';
+export default {
+  components: { OnePost },
+    setup(){
+        const posts=ref([
+            {
+            "id": 1,
+            "title": "Learn angular",
+            "content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis doloribus quis quam nulla, nihil dignissimos rerum, doloremque tempora perferendis est nostrum tempore corrupti libero commodi quae voluptate qui delectus voluptatum!"
+            },
+            {
+            "id": 2,
+            "title": "Learn Flutter",
+            "content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis doloribus quis quam nulla, nihil dignissimos rerum, doloremque tempora perferendis est nostrum tempore corrupti libero commodi quae voluptate qui delectus voluptatum!"
+            },
+            {
+            "id": 3,
+            "title": "Learn vuejs",
+            "content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis doloribus quis quam nulla, nihil dignissimos rerum, doloremque tempora perferendis est nostrum tempore corrupti libero commodi quae voluptate qui delectus voluptatum!"
+            },
+            ]);
+            return {posts}
+    }
+}
+</script>
+
+<style>
+
+</style>
